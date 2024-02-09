2a729f25e60815db9e058c6fd934d74916a85d42
Rocio <rciperezmlina1@gmail.com>
2024-02-09 13:08:21 +0100

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   src/app/components/terminos-del-servicio/terminos-del-servicio.component.css
    	new file:   src/app/components/terminos-del-servicio/terminos-del-servicio.component.html
    	new file:   src/app/components/terminos-del-servicio/terminos-del-servicio.component.spec.ts
    	new file:   src/app/components/terminos-del-servicio/terminos-del-servicio.component.ts

---
+++ src/app/components/terminos-del-servicio/terminos-del-servicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terminos-del-servicio',
+  standalone: true,
+  imports: [],
+  templateUrl: './terminos-del-servicio.component.html',
+  styleUrl: './terminos-del-servicio.component.css'
+})
+export class TerminosDelServicioComponent {
+
+}
